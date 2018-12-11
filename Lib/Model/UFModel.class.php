b834283270c628b3716c195f4c25d22cba60db45
Liangsf <lsf880101@foxmail.com>
2018-12-11 14:49:21 +0800

    uuid

--- Lib/Model/UFModel.class.php
+++ Lib/Model/UFModel.class.php
@@ -18,7 +18,7 @@ class UFModel extends CommonModel {
 		$rs = $this->alias('a')
 					->join(' xz_wx_users as u ON a.open_id = u.open_id')
 					->join(' xz_affairs as af ON a.affair_id = af.id')
-					->field('a.join_time, a.open_id, a.sign_time, a.out_trade_no, a.pay_time, a.status as join_status, u.nickname, u.name, u.avatarurl, u.mobile, af.id, af.active_time, af.close_time, af.address, af.address_Lng, af.address_Lat, af.promise_money, af.quota, af.adr_name, af.title, af.content, af.status')
+					->field('a.join_time, a.open_id, a.sign_time, a.out_trade_no, a.pay_time, a.status as join_status, u.nickname, u.name, u.avatarurl, u.mobile, af.id, af.active_time, af.close_time, af.address, af.address_Lng, af.address_Lat, af.promise_money, af.quota, af.adr_name, af.title, af.content, af.status, u.uuid')
 					->where($where)
 					->page($page, $pageSize)
 					->order('af.active_time asc')
