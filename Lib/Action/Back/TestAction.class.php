67031c2fb733ed6aaf6be13bbbcaabad35339e69
Liangsf <lsf880101@foxmail.com>
2018-12-18 10:39:24 +0800

    dd

--- Lib/Action/Back/TestAction.class.php
+++ Lib/Action/Back/TestAction.class.php
@@ -486,4 +486,30 @@ class TestAction extends Action
         var_dump($ok);
     }
 
+    /**
+     * 下载对账单
+     * @return [type] [description]
+     */
+    public function downloadBills()
+    {
+        $_REQUEST["bill_date"] = '20181215';
+        $_REQUEST["bill_type"] = 'ALL';
+        if(isset($_REQUEST["bill_date"]) && $_REQUEST["bill_date"] != ""){
+
+        	$bill_date = $_REQUEST["bill_date"];
+            $bill_type = $_REQUEST["bill_type"];
+        	$input = new WxPayDownloadBill();
+        	$input->SetBill_date($bill_date);
+        	$input->SetBill_type($bill_type);
+        	$config = new WxPayConfig();
+        	$file = WxPayApi::downloadBill($config, $input);
+
+        	//echo  htmlspecialchars($file, ENT_QUOTES);
+        	//TODO 对账单文件处理
+            $format_res = deal_WeChat_response($file);
+            print_r($format_res);
+            exit(0);
+        }
+    }
+
 }
